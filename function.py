ba306f62b840d3e1d913bfe1bda0b1a0045f988e
DEMONER <limjuja9292@gmail.com>
2026-02-19 16:38:11 +0900

    Add percenter function to calculate situation match percentage

--- function.py
+++ function.py
@@ -3,3 +3,20 @@ def find_emotion(data, situation):
         if record["상황"] == situation:
             return record["감정"]
     return None
+
+def percenter(text, situation):
+    text_list = list(text)
+    situation_list = list(situation)
+    mc = 0
+    for x in situation_list:
+        if x in text_list:
+            mc += 1
+            text_list.remove(x)
+
+    p = len(situation)
+
+    if len(situation) == 0:
+        return 0
+
+    percent = mc / p * 100
+    return percent
